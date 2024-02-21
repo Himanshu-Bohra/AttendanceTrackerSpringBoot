16c0b45990f14927145cfc27e5dfc468b8d474d7
Himanshu-Bohra <himanshubohra@hotmail.com>
2024-02-21 12:18:41 -0500

    First commit
    Creating the attendance tracker project (intelliJ)

---
+++ src/main/java/com/example/attendancetrackerspringboot/AttendanceTrackerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.attendancetrackerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceTrackerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceTrackerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/attendancetrackerspringboot/AttendanceTrackerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.attendancetrackerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceTrackerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
